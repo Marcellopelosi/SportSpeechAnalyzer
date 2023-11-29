c9b46e0ebebbe390f02f222af7a53c341da611a2
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-29 21:02:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from PuntuactionCorrector
-from KeywordsExtractor
+import PuntuactionCorrector
+import KeywordsExtractor
 
 def main():
     st.title("Keywords Extraction Interface")
